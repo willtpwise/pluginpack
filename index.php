3c8425cba673d09d2e1e76dada2627393cfa8eb5
William Wise <williamwise@Will-Wise.local>
2017-08-24 07:29:29 +1000

    Syntax error with double quotes:

--- index.php
+++ index.php
@@ -8,18 +8,18 @@
 
   Built using [pluginpack](https://github.com/willtpwise/pluginpack)
 */
-require __DIR__ . "/inc/environment.php";
-require __DIR__ . "/inc/dependencies.php";
+require __DIR__ . '/inc/environment.php';
+require __DIR__ . '/inc/dependencies.php';
 
 // Delete the lines below, once your ready.
 function plugin_pack_well_done() {
   ?>
-  <div class="updated notice">
-    <p>You"ve successfull installed the plugin pack.<br>
+  <div class='updated notice'>
+    <p>You've successfull installed the plugin pack.<br>
 			Now head to <code>plugin-pack/index.php</code> and start your plugin.</p>
   </div>
   <?php
 }
-add_action("admin_notices", "plugin_pack_well_done");
-add_action("network_admin_notices", "plugin_pack_well_done");
+add_action('admin_notices', 'plugin_pack_well_done');
+add_action('network_admin_notices', 'plugin_pack_well_done');
 
